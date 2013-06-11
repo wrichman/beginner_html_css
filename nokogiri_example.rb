dd9b920f7f117e934b2ec9ea65f3d5f8a7107b17
wrichman <williamrichman@gmail.com>
2013-06-11 13:27:05 -0400

    Fixed header and footer

--- nokogiri_example.rb
+++ nokogiri_example.rb
@@ -1,33 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
 
-# wrap in html, head, body tags
-
-def header
-  "<html>\n"
-    "<head>\n"
-      "<title>Intro to HTML/CSS</title>\n"
-      "<link rel='stylesheet' type='text/css' href='positioning_style.css'>\n"
-    "</head>\n"
-    "<body>\n"
-end
-
-def footer
-    "</body>\n"
-  "</html>\n"
-end
-
-header
+page = Nokogiri::HTML(open("http://toronto.en.craigslist.ca/bia/"))
+bikes = page.css('blockquote#toc_rows p.row span.pl a')
 
 File.open('bikes.html', 'w') do |f|
-  page = Nokogiri::HTML(open("http://toronto.en.craigslist.ca/bia/"))
-  bikes = page.css('blockquote#toc_rows p.row span.pl a')
-  f.write(header)
+  
+  f.write("<html>\n")
+  f.write("<head>\n")
+  f.write("<title>Intro to HTML/CSS</title>\n")
+  f.write("<link rel='stylesheet' type='text/css' href='positioning_style.css'>\n")
+  f.write("</head>\n")
+  f.write("<body>\n")
+
   bikes.each do |bike|
     f.write("<br>")
     f.write("<div>" + bike.text() + "</div>\n")
   end
-  f.write(footer)
-end
 
-footer
+  f.write("</body>\n")
+  f.write("</html>\n")
+end
